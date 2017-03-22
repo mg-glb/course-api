17c80b9b44167c13b993bd8985ce234ec06700fa
m.gigena <m.gigena@globant.com>
2017-03-22 17:54:52 -0300

    Initial commit

---
+++ src/main/java/com/globant/springbootquickstart/hello/Controller.java
@@ -0,0 +1,12 @@
+package com.globant.springbootquickstart.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+  @RequestMapping("/hello")
+  public String sayHi() {
+    return "Hi";
+  }
+}
